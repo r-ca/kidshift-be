0e4ed4e7c4af28780ec2cde39d23089ba7275bbd
rca <nem@nem.x0.to>
2024-06-16 21:41:31 +0900

    コメント, レスポンスコード修正, ログ追加

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,27 +1,34 @@
 import { Router, Request, Response } from 'express';
 import { registUser, loginUser } from '@src/services/authService'; 
+import Logger from '@src/logger';
 
 const router = Router();
+const logger = new Logger();
+logger.setTag('authRouter');
 
 router.post('/register', (req: Request, res: Response) => {
     const { email, password } = req.body;
     registUser(email, password)
-        .then((user) => { // トークンとかを返すべき
+        .then((user) => {
             res.json(user);
         })
         .catch((err) => {
-            res.status(500).json({ message: err.message });
+            res.status(401).json({ message: "ユーザー登録失敗: すでに登録されているemailです" });
+            logger.warn("Regist user failed");
+            logger.debug(err.message);
         });
 });
 
 router.post('/login', (req: Request, res: Response) => {
     const { email, password } = req.body;
     loginUser(email, password)
-        .then((user) => { // トークンとかを返すべき
+        .then((user) => {
             res.json(user);
         })
         .catch((err) => {
-            res.status(500).json({ message: err.message });
+            res.status(401).json({ message: "ログイン失敗: emailかpasswordが間違っています" });
+            logger.warn("Login failed");
+            logger.debug(err.message);
         });
 });
 
