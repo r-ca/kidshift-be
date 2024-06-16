2330f7404ec8a639eb1b592fed89b30a2489c88f
rca <nem@nem.x0.to>
2024-06-16 15:17:06 +0900

    トークンを検証するミドルウェアを追加

---
+++ src/routers/middlewares/verifyToken.ts
@@ -0,0 +1,23 @@
+import jsonwebtoken from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export default function verifyToken(req: Request, res: Response, next: NextFunction) {
+    const authorizationHeader = req.headers["authorization"];
+    let token;
+    if (authorizationHeader) {
+        const token_ = authorizationHeader.split(" ")[1];
+        if (token_) {
+            token = token_;
+        } else {
+            return res.status(401).send("アクセス拒否: アクセストークンが必要なエンドポイントです");
+        }
+    } else {
+        return res.status(401).send("アクセス拒否: アクセストークンが必要なエンドポイントです");
+    }
+    try {
+        jsonwebtoken.verify(token, "secret"); // TODO: ハードコードやめる
+        next();
+    } catch (error) {
+        return res.status(401).send("アクセス拒否: トークンの検証に失敗しました(有効期限切れか、不正なトークンです)");
+    }
+}
