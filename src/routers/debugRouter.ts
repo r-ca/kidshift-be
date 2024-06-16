6287d3f4d817239dcbb93e5ff11d2ba7b5116db7
rca <nem@nem.x0.to>
2024-06-16 15:24:21 +0900

    debugRouterに検証用エンドポイント追加

--- src/routers/debugRouter.ts
+++ src/routers/debugRouter.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
+import verifyToken from '@middlewares/verifyToken';
 
 const router = Router();
 
 router.get('/hello', (req: Request, res: Response) => {
     res.send('Hello, World!');
+});
+
+router.get('/protected', verifyToken,(req: Request, res: Response) => {
+    res.send('You are authorized to access this endpoint');
 });
 
 router.post('/echo', (req: Request, res: Response) => {
