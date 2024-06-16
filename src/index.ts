dc5f329bb8f9b35107332204b27f1b714000071d
rca <nem@nem.x0.to>
2024-06-16 16:42:08 +0900

    greet追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import Logger from "@src/logger";
 import debugRouter from "@src/routers/debugRouter";
+import prisma from "@src/prisma";
+import os from "os";
+
 
 const app = express();
 const port = 3000;
@@ -8,12 +11,43 @@ const port = 3000;
 const logger = new Logger();
 logger.setTag("index.ts");
 
+// Greetings
+console.log(`
+      :::    :::       :::::::::::       :::::::::       ::::::::       :::    :::       :::::::::::       ::::::::::   :::::::::::
+     :+:   :+:            :+:           :+:    :+:     :+:    :+:      :+:    :+:           :+:           :+:              :+:
+    +:+  +:+             +:+           +:+    +:+     +:+             +:+    +:+           +:+           +:+              +:+
+   +#++:++              +#+           +#+    +:+     +#++:++#++      +#++:++#++           +#+           :#::+::#         +#+
+  +#+  +#+             +#+           +#+    +#+            +#+      +#+    +#+           +#+           +#+              +#+
+ #+#   #+#            #+#           #+#    #+#     #+#    #+#      #+#    #+#           #+#           #+#              #+#
+###    ###       ###########       #########       ########       ###    ###       ###########       ###              ###`);
+
+console.log("\n");
+console.log("==Environment================================================================================================");
+
+console.log("Environmnet:");
+console.log("  System:");
+console.log("    OS: " + os.type() + " " + os.release());
+console.log("    CPU: " + os.cpus()[0].model + " x" + os.cpus().length);
+console.log("    Memory: " + os.totalmem() / 1024 / 1024 / 1024 + " GB");
+console.log("    Platform: " + os.platform());
+console.log("  Software:");
+console.log("    Node.js: " + process.versions.node);
+console.log("    V8: " + process.versions.v8);
+console.log("  Configuration:");
+console.log("    Port: " + port);
+
+console.log("==============================================================================================================");
+console.log("\n");
+
+logger.info("Starting server...");
+
 app.get("/", (req, res) => {
     res.status(200).sendFile("index.html", { root: "static" });
 });
 
 app.use(express.json());
 app.use('/debug', debugRouter);
+
 
 app.listen(port, () => {
     logger.info(`Server started! Listening on port ${port}`);
