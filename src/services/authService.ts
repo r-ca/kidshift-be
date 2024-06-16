454403d83e4a1839d6a86e54268c67371ffc232f
rca <nem@nem.x0.to>
2024-06-16 21:06:05 +0900

    戻り値をIDに

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,12 +1,19 @@
+import { Prisma, PrismaClient, User } from "@prisma/client";
 import prisma from "@src/prisma";
 
-function registUser(email: string, password: string) {
-    return prisma.user.create({ // TODO: バリデーション, ハッシュ化, その他
+
+function registUser(email: string, password: string): Promise<String> {
+    
+    const registUser = prisma.user.create({ // TODO: バリデーション, ハッシュ化, その他
         data: {
             email,
             password,
             name: email,
         }
+    });
+
+    return registUser.then((user) => {
+        return user.id
     });
 }
 
