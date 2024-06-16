ca25a6f957890d1de3c284e9d13401da94062276
rca <nem@nem.x0.to>
2024-06-16 16:47:30 +0900

    debugRouterに追加

--- src/routers/debugRouter.ts
+++ src/routers/debugRouter.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import verifyToken from '@middlewares/verifyToken';
+import { issueTokenByUserId } from '@src/utils/tokenUtils';
 
 const router = Router();
 
@@ -7,8 +8,14 @@ router.get('/hello', (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
 
+router.post('bypassAuth', (req: Request, res: Response) => {
+    const userId = req.body.userId;
+    const token = issueTokenByUserId(userId);
+    res.json({ token });
+});
+
 router.get('/protected', verifyToken,(req: Request, res: Response) => {
-    res.send('You are authorized to access this endpoint');
+    res.send('You are authorized to access this endpoint. user id: ' + req.body.userId);
 });
 
 router.post('/echo', (req: Request, res: Response) => {
