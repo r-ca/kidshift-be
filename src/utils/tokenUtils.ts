20ca116e0a23b3c0649b0da46252b8672cdf1363
rca <nem@nem.x0.to>
2024-06-18 01:51:09 +0900

    クレームにロールを含めるように

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -1,11 +1,21 @@
 import jsonwebtoken from "jsonwebtoken";
+import { Role } from "@src/enums";
 
 function issueToken(payload: object) {
     return jsonwebtoken.sign(payload, "secret");
 }
 
 function issueTokenByUserId(userId: string) {
-    return issueToken({ userId });
+    const payload = {
+        userId: userId,
+        role: Role.PARENT
+    };
+
+    const options = {
+        expiresIn: "9999h" // TODO: あまりにも長過ぎる
+    };
+
+    return jsonwebtoken.sign(payload, "secret", options);
 }
 
 export { issueTokenByUserId };
