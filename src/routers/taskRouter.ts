167febf17accefe6a9343cf615813dd945e59e60
ろむねこ <nem@nem.x0.to>
2024-06-17 10:30:20 +0900

    get / 実装

--- src/routers/taskRouter.ts
+++ src/routers/taskRouter.ts
@@ -12,4 +12,43 @@ router.get('/', (req, res) => {
         });
         return;
     }
+    if (req.query.childId && req.query.userId) {
+        res.status(400).json({
+            message: '不正なリクエスト: childIdとuserIdはどちらか一方を指定してください'
+        });
+        return;
+    }
+    if (!req.query.childId && !req.query.userId) {
+        res.status(400).json({
+            message: '不正なリクエスト: childIdかuserIdのどちらかを指定してください'
+        });
+        return;
+    }
+    if (req.query.childId) {
+        getTasksByChildId(req.query.childId as string)
+            .then((tasks: Task[]) => {
+                res.status(200).json(tasks);
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    message: 'エラーが発生しました',
+                    error: err
+                });
+            });
+        return;
+    } else {
+        getTasksByUserId(req.query.userId as string)
+            .then((tasks: Task[]) => {
+                res.status(200).json(tasks);
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    message: 'エラーが発生しました',
+                    error: err
+                });
+            });
+        return;
+    }
 });
+
+
