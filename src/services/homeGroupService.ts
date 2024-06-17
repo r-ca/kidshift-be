51fe6ab7129a5b63ef5d8d1849a40b2f2865c909
rca <nem@nem.x0.to>
2024-06-18 02:41:35 +0900

    homegroupをつくるメソッドを追加

--- src/services/homeGroupService.ts
+++ src/services/homeGroupService.ts
@@ -1,1 +1,12 @@
+import { HomeGroup, PrismaPromise } from "@prisma/client";
+import prisma from "@src/prisma";
 
+function createHomeGroup(name: string): PrismaPromise<HomeGroup> {
+    return prisma.homeGroup.create({
+        data: {
+            name
+        }
+    });
+}
+
+export { createHomeGroup };
