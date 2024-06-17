dea1aad7f148421278f9f5507e42aa15e2df75dd
rca <nem@nem.x0.to>
2024-06-18 02:44:10 +0900

    homeGroupIdが指定されてないときは作成するように

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,16 +1,22 @@
 import prisma from "@src/prisma";
 import bcrypt from "bcrypt";
 import { issueTokenByUserId } from "@src/utils/tokenUtils";
+import { createHomeGroup } from "@src/services/homeGroupService";
 
-async function registUser(email: string, password: string): Promise<String> {
+async function registUser(email: string, password: string, homeGroupId?: string): Promise<String> {
     
     const hashedPassword = bcrypt.hashSync(password, 10);
+
+    if (!homeGroupId) {
+        homeGroupId = await createHomeGroup(email).then((homeGroup) => { return homeGroup.id });
+    }
 
     const registUser = prisma.user.create({ // TODO: emailバリデーション
         data: {
             email,
             password: hashedPassword,
             display_name: email,
+            home_group_id: homeGroupId
         }
     });
 
