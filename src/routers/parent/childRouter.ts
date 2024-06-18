712d1535b70085a8a04a968c03b7d3dc16c5d150
ろむねこ <nem@nem.x0.to>
2024-06-18 19:04:20 +0900

    子供一覧取得を実装

--- src/routers/parent/childRouter.ts
+++ src/routers/parent/childRouter.ts
@@ -1,11 +1,27 @@
 import { Router, Request, Response } from 'express';
+import { getChilds, createChild } from '@src/services/childService';
+import Logger from '@src/logger';
 
 const router = Router();
+const logger = new Logger();
+logger.setTag('parent/childRouter');
 
 router.get('/', (req: Request, res: Response) => {
-    // 子供の一覧を取得
-    res.status(501).json({
-        message: 'WIP'
+    if (!req.user) {
+        return res.status(500).json({
+            message: 'エラーが発生しました(JWT解析結果が不正/未設定です)'
+        });
+    }
+    const homeGroupId = req.user.claims.home_group_id;
+    getChilds(homeGroupId).then((children) => {
+        res.status(200).json(children);
+    }).catch((err) => {
+        logger.error('Failed to get children')
+        logger.debug(err);
+        res.status(500).json({
+            message: 'エラーが発生しました',
+            detail: err
+        });
     });
 });
 
