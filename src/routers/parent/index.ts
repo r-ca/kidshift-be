3e9662178f722159198b01d5ab231f23de2572db
ろむねこ <nem@nem.x0.to>
2024-06-18 12:59:46 +0900

    accountRouterをマウント

--- src/routers/parent/index.ts
+++ src/routers/parent/index.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import Logger from '@src/logger';
 import verifyToken from '../middlewares/verifyToken';
 
-
 import authRouter from './authRouter';
 import taskRouter from './taskRouter';
+import accountRouter from './accountRouter';
 import verifyParent from '../middlewares/verifyParent';
 
 const router = Router();
@@ -13,5 +13,6 @@ logger.setTag('parent/index.ts');
 
 router.use('/auth', authRouter);
 router.use('/task', verifyToken, verifyParent, taskRouter);
+router.use('/account', verifyToken, verifyParent, accountRouter);
 
 export default router;
