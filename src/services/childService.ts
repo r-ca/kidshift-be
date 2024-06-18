fb9310c4c0630f5c5eb8b7fbaeacbf4b1917aa60
ろむねこ <nem@nem.x0.to>
2024-06-18 19:00:50 +0900

    addChild作成

--- src/services/childService.ts
+++ src/services/childService.ts
@@ -9,5 +9,12 @@ async function getChilds(homeGroupId: string): Promise<Child[]> {
     }).then((children) => { return children; });
 }
 
+async function addChild(childName: string): Promise<Child> {
+    const child: Child = {} as Child;
+    child.name = childName;
+    return prisma.child.create({
+        data: child
+    }).then((child) => { return child; });
+}
 
-export { getChilds };
+export { getChilds, addChild };
