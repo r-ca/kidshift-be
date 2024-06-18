caf7d66bda71ec93f69acb2db645b576da10f449
ろむねこ <nem@nem.x0.to>
2024-06-18 18:59:03 +0900

    add migrate

---
+++ prisma/migrations/20240618095848_delete_linked_parent/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `linked_user_id` on the `Child` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Child" DROP COLUMN "linked_user_id";
