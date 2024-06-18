1e25f940f7aa619d4e0e596751d2d1a409e5f2f9
ろむねこ <nem@nem.x0.to>
2024-06-18 13:00:08 +0900

    リファクタ

--- src/routers/parent/index.ts
+++ src/routers/parent/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import Logger from '@src/logger';
 import verifyToken from '../middlewares/verifyToken';
+import verifyParent from '../middlewares/verifyParent';
 
 import authRouter from './authRouter';
 import taskRouter from './taskRouter';
 import accountRouter from './accountRouter';
-import verifyParent from '../middlewares/verifyParent';
 
 const router = Router();
 const logger = new Logger();
