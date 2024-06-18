d2f34d30f4a7dec8165e7a8743e3a4be68b99bd8
ろむねこ <nem@nem.x0.to>
2024-06-18 12:41:02 +0900

    ミドルウェアを読込

--- src/routers/parent/index.ts
+++ src/routers/parent/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import Logger from '@src/logger';
+import verifyToken from '../middlewares/verifyToken';
+
 
 import authRouter from './authRouter';
 import taskRouter from './taskRouter';
+import verifyParent from '../middlewares/verifyParent';
 
 const router = Router();
 const logger = new Logger();
 logger.setTag('parent/index.ts');
 
 router.use('/auth', authRouter);
-router.use('/task', taskRouter);
+router.use('/task', verifyToken, verifyParent, taskRouter);
 
 export default router;
