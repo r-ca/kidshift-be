c162e0f066ab6d1b96b63f7165d2fc57762edd89
ろむねこ <nem@nem.x0.to>
2024-06-19 16:06:01 +0900

    loginエンドポイント追加

--- src/routers/child/authRouter.ts
+++ src/routers/child/authRouter.ts
@@ -1,7 +1,17 @@
 import { Router, Request, Response } from 'express';
+import { login } from '@src/services/child/authService';
 
 const router = Router();
 
 // login
 router.post('/login', (req: Request, res: Response) => {
-    
+    const code: number = req.body.code;
+    if (!code) {
+        return res.status(400).json({ message: 'ログインコードが指定されていません' });
+    }
+    login(code).then((token) => {
+        res.status(200).json({ accessToken: token });
+    }).catch((err) => {
+        res.status(500).json({ message: 'ログインに失敗しました', detail: err });
+    });
+});
