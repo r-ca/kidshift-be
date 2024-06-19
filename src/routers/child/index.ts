08cb00a788c8a23a848bc6ebf3433d5cdf0a13c6
ろむねこ <nem@nem.x0.to>
2024-06-19 16:06:30 +0900

    マウント

--- src/routers/child/index.ts
+++ src/routers/child/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import taskRouter from './taskRouter';
+import authRouter from './authRouter';
 
 const router = Router();
 
 router.use('/task', taskRouter);
+router.use('/auth', authRouter);
 
 export default router;
