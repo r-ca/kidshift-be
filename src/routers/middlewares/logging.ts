f7d16543cecfe112bc60057abff0702fb30cf7ea
ろむねこ <nem@nem.x0.to>
2024-06-19 16:57:24 +0900

    修正

--- src/routers/middlewares/logging.ts
+++ src/routers/middlewares/logging.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 export default function logging(req: Request, res: Response, next: NextFunction) {
     const logger = getRequestLogger();
     logger.info(`[${req.method}] ${req.originalUrl}`);
-    logger.debug(`Request headers: ${JSON.stringify(req.headers)}`);
+    logger.debug(`Request params: ${JSON.stringify(req.params)}`);
     logger.debug(`Request body: ${JSON.stringify(req.body)}`);
     next();
 }
