352e824f013e8f6bed6afb3d9b03285b521fdc1c
rca <nem@nem.x0.to>
2024-06-23 19:20:41 +0900

    非同期関係の実装ミスを修正

--- src/routers/parent/accountRouter.ts
+++ src/routers/parent/accountRouter.ts
@@ -1,21 +1,34 @@
 import { Router, Request, Response } from 'express';
 import { findUserById } from '@src/utils/userUtils';
-const router = Router();
+import Logger from '@src/logger';
 
+const router = Router();
+const logger = new Logger();
+logger.setTag("accountRouter");
 // Get userinfo
 router.get('/', (req: Request, res: Response) => {
-    if (!req.user) {
+    if (!req.user || !req.user.claims || !req.user.claims.sub) {
         return res.status(500).json({
             message: 'エラーが発生しました(JWT解析結果が不正/未設定です)'
         });
     }
-    const user = findUserById(req.user.claims.sub);
-    if (!user) {
-        return res.status(404).json({
-            message: 'ユーザが見つかりません'
-        });
-    }
-    res.status(200).json(user);
+    findUserById(req.user.claims.sub).then(user => {
+        if (!user) {
+            return res.status(404).json({
+                message: 'ユーザが見つかりません'
+            });
+        }
+        logger.debug(`User info: ${JSON.stringify(user)}`);
+        res.status(200).json(user);
+    }).catch(err => {
+            logger.error('Failed to fetch user info');
+            logger.debug(`Error: ${err}`);
+            res.status(500).json({
+                message: 'エラーが発生しました',
+                detail: err
+            });
+        }
+        );
 });
 
 router.put('/', (req: Request, res: Response) => {
