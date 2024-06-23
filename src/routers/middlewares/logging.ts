5c18ac6211b79fd89c6ea578af5271d24782975d
rca <nem@nem.x0.to>
2024-06-23 15:25:53 +0900

    ヘッダーをログに表示するように

--- src/routers/middlewares/logging.ts
+++ src/routers/middlewares/logging.ts
@@ -6,5 +6,6 @@ export default function logging(req: Request, res: Response, next: NextFunction)
     logger.info(`[${req.method}] ${req.originalUrl}`);
     logger.debug(`Request params: ${JSON.stringify(req.params)}`);
     logger.debug(`Request body: ${JSON.stringify(req.body)}`);
+    logger.debug(`Request headers: ${JSON.stringify(req.headers)}`);
     next();
 }
