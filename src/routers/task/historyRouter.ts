41bdfb9ee41a86c131672c140c3e6a948766db43
ろむねこ <nem@nem.x0.to>
2024-07-03 14:53:16 +0900

    ひな形追加

--- src/routers/task/historyRouter.ts
+++ src/routers/task/historyRouter.ts
@@ -1,5 +1,19 @@
 import { Router } from 'express';
 
-const router = Router();
+const parentRouter = Router(); // 親専用エンドポイント
+const commonRouter = Router(); // 共用エンドポイント
 
-export default router;
+commonRouter.get("/", (req, res) => {
+    res.status(501).send("WIP");
+    // TODO: 履歴一覧
+});
+
+parentRouter.delete("/:historyId", (req, res) => {
+    res.status(501).send("WIP");
+    // TODO: 履歴削除
+});
+
+parentRouter.post("/", (req, res) => {
+    res.status(501).send("WIP");
+    // TODO: 履歴手動追加
+});
