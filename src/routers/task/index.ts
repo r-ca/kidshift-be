ed4df77775c908320b322817965630f53d290998
ろむねこ <nem@nem.x0.to>
2024-07-03 14:58:54 +0900

    リネーム

--- src/routers/task/index.ts
+++ src/routers/task/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { commonRouter, parentRouter } from './rootRouter';
+import { commonRouter as rootCommonRouter, parentRouter as rootParentRouter } from './rootRouter';
 import { commonRouter as historyCommonRouter, parentRouter as historyParentRouter } from './historyRouter';
 import verifyToken from '../middlewares/verifyToken';
 import verifyParent from '../middlewares/verifyParent';
 
 const router = Router();
 
-router.use('/', verifyToken, commonRouter);
-router.use('/' , verifyToken, verifyParent, parentRouter);
+router.use('/', verifyToken, rootCommonRouter);
+router.use('/' , verifyToken, verifyParent, rootParentRouter);
 router.use('/history', verifyToken, historyCommonRouter);
 router.use('/history', verifyToken, verifyParent, historyParentRouter);
 
