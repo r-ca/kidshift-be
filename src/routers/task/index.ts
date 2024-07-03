ab0ad6cd66df3a462c0c928f486cde98f7632580
ろむねこ <nem@nem.x0.to>
2024-07-03 14:49:25 +0900

    ミドルウェア追加

--- src/routers/task/index.ts
+++ src/routers/task/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { commonRouter, parentRouter } from './rootRouter';
+import verifyToken from '../middlewares/verifyToken';
+import verifyParent from '../middlewares/verifyParent';
 
 const router = Router();
 
-router.use('/', commonRouter);
-router.use('/' , parentRouter);
+router.use('/', verifyToken, commonRouter);
+router.use('/' , verifyToken, verifyParent, parentRouter);
 
 export default router;
 
