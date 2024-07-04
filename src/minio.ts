712f83f6287753cddd78c5150a3dd75d8cf1e626
rca <nem@nem.x0.to>
2024-07-04 23:22:54 +0900

    client追加

---
+++ src/minio.ts
@@ -0,0 +1,11 @@
+import * as Minio from 'minio';
+
+const minioClient = new Minio.Client({
+    endPoint: 'localhost',
+    port: 9000,
+    useSSL: false,
+    accessKey: "bCNKDBVU1CJz9XkDqhB0",
+    secretKey: "gnrpzGimjKn3WQmxmvG5IRDpcMZZixDaXjFy9mtz"
+});
+
+export { minioClient };
