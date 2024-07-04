12ba1bf2652c7dbaea2214cd2a0d2b17ec59bd0c
ろむねこ <nem@nem.x0.to>
2024-07-04 10:31:30 +0900

    定数を使ってレスポンスするように

--- src/routers/middlewares/verifyParent.ts
+++ src/routers/middlewares/verifyParent.ts
@@ -1,18 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { Role } from "@src/enums";
+import { jwtVerifyErrorResponse, notPermittedResponse } from "@src/models/commons/responses";
 
 export default function verifyParent(req: Request, res: Response, next: NextFunction) {
     if (req.user) {
         if (req.user.claims.role === Role.PARENT) {
             next();
         } else {
-            res.status(401).json({
-                message: '権限がありません(保護者のみ利用可能なAPIです)'
-            });
+            res.status(notPermittedResponse().statusCode).json(notPermittedResponse().body);
         }
     } else {
-        res.status(500).json({
-            message: 'エラーが発生しました(JWT解析結果が不正/未設定です)'
-        });
+        res.status(jwtVerifyErrorResponse().statusCode).json(jwtVerifyErrorResponse().body);
     }
 }
