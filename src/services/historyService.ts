05a06e8232e36c7d8be736e7bfd3f48747999714
ろむねこ <nem@nem.x0.to>
2024-07-08 10:01:49 +0900

    Service実装 WIP

---
+++ src/services/historyService.ts
@@ -0,0 +1,24 @@
+import { HistoryResponse } from "@src/models/History";
+import prisma from "@src/prisma";
+
+async function getHistories(childId: string): Promise<HistoryResponse[]> {
+    return prisma.taskCompletion.findMany({
+        where: {
+            child_id: childId,
+        },
+    }).then((histories) => {
+        if (!histories) {
+            return [];
+        }
+        return histories.map((history) => {
+            return {
+                id: history.id,
+                childId: history.child_id,
+                taskId: history.task_id,
+                registeredAt: history.registerd_at
+            };
+        });
+    });
+}
+
+export { getHistories };
