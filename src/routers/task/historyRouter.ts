d9627b2ca493271e9f91071648eeb8b63a3853eb
ろむねこ <nem@nem.x0.to>
2024-07-08 10:08:21 +0900

    ルーター実装

--- src/routers/task/historyRouter.ts
+++ src/routers/task/historyRouter.ts
@@ -1,11 +1,25 @@
+import { notFoundResponse, requiredFieldMissingResponse } from '@src/models/commons/responses';
+import { HistoryListResponse, HistoryResponse } from '@src/models/History';
+import { getHistories } from '@src/services/historyService';
 import { Router } from 'express';
 
 const parentRouter = Router(); // 親専用エンドポイント
 const commonRouter = Router(); // 共用エンドポイント
 
-commonRouter.get("/", (req, res) => {
-    res.status(501).send("WIP");
-    // TODO: 履歴一覧
+commonRouter.get("/:childId", (req, res) => {
+    const childId = req.params.childId;
+    if (!childId) {
+        const childIdMissingResponse = requiredFieldMissingResponse(["childId"]);
+        res.status(childIdMissingResponse.statusCode).send(childIdMissingResponse.body);
+    }
+    getHistories(childId).then((histories: HistoryResponse[]) => {
+        if (!histories || histories.length === 0) {
+            res.status(notFoundResponse().statusCode).send(notFoundResponse().body);
+        }
+        res.status(200).send({
+            list: histories,
+        } as HistoryListResponse);
+    });
 });
 
 parentRouter.delete("/:historyId", (req, res) => {
