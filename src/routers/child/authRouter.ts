cd04235e6df1ed24fd51b655e81e1707f7617522
rca <nem@nem.x0.to>
2024-07-09 01:43:55 +0900

    型修正

--- src/routers/child/authRouter.ts
+++ src/routers/child/authRouter.ts
@@ -5,11 +5,11 @@ const router = Router();
 
 // login
 router.post('/login', (req: Request, res: Response) => {
-    const code: number = req.body.code;
-    if (!code) {
+    const loginCode: string = req.body.loginCode;
+    if (!loginCode) {
         return res.status(400).json({ message: 'ログインコードが指定されていません' });
     }
-    login(code).then((token) => {
+    login(loginCode).then((token) => {
         res.status(200).json({ accessToken: token });
     }).catch((err) => {
         res.status(500).json({ message: 'ログインに失敗しました', detail: err });
